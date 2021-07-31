2adf9b272b51cf93168e3d0a918aba240dde6221
zhanghongyi1996 <48501077+zhanghongyi1996@users.noreply.github.com>
2021-07-31 23:11:43 +0100

    Add files via upload

---
+++ Get_close_to_real_application/One_layer_deployment_improvement_find_all_boxes/test.py
@@ -0,0 +1,23 @@
+import numpy as np
+import math
+import random
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import scipy
+from scipy.spatial.distance import cdist, pdist, euclidean
+import warehouse
+import pickle
+import sys
+import os
+import seaborn as sns
+import product_finding
+
+swarm_size = 10
+ani = True
+time_limit = 30000
+num_boxes = 10
+scan_range = 36
+width = 1000
+height = 1000
+product_finding.data(swarm_size,ani,time_limit,num_boxes,scan_range,width,height)

5b33a3c41cf2f65d72db229c4858fa5d4e81a449
zhanghongyi1996 <48501077+zhanghongyi1996@users.noreply.github.com>
2021-07-31 23:17:32 +0100

    Add files via upload

---
+++ Get_close_to_real_application/Two_layer_deployment_improvement_find_one_random_selected_box/testfile.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+import random
+import matplotlib
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import scipy
+from scipy.spatial.distance import cdist, pdist, euclidean
+import warehouse
+import pickle
+import sys
+import os
+import find_one_object_can_pass_through_the_box
+
+swarm_size = 10
+ani = True
+time_limit = 300000
+num_boxes = 10
+scan_range = 40
+width = 1000
+height = 1000
+find_one_object_can_pass_through_the_box.data(swarm_size,ani,time_limit,num_boxes,scan_range,width,height)
